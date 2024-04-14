f6554923e67580d80a6f1704e9a17b7a4cf6bdb0
Rafter148 <165582023+Rafter148@users.noreply.github.com>
2024-04-15 00:45:40 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = "20544260"
-api_hash = "a0b00461d3fba22aa186fa648d77787e"
-bot_token = "6940355682:AAEaTbv9tLz7ZKOEcMJ1HBioIP2goQOdQSY"
-sudo_groups = -1002051259332
+api_id = "27006142"
+api_hash = "8d5171c23fd1836db908b8739d288336"
+bot_token = "7097347933:AAEb_4UBJ_SeDSTa7zoIeEPnsFTcfs7F-OE"
+sudo_groups = -100212046048
